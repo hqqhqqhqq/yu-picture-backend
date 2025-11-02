4ecae89a261a287df8d2d7b2b90ce76e172e170b
hqqhqqhqq <764238492@qq.com>
2025-11-02 17:05:38 +0800

    设计空间模块

---
+++ src/main/java/com/yupi/yupicturebackend/service/SpaceService.java
@@ -0,0 +1,13 @@
+package com.yupi.yupicturebackend.service;
+
+import com.yupi.yupicturebackend.model.entity.Space;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author hqqhqq
+* @description 针对表【space(空间)】的数据库操作Service
+* @createDate 2025-11-02 16:48:07
+*/
+public interface SpaceService extends IService<Space> {
+
+}
