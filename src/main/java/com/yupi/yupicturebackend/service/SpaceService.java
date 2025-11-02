2104a3c9916bea64b89a3035bdf3bcad51f01a73
hqqhqqhqq <764238492@qq.com>
2025-11-02 23:00:45 +0800

    完成空间模块的基础功能开发

--- src/main/java/com/yupi/yupicturebackend/service/SpaceService.java
+++ src/main/java/com/yupi/yupicturebackend/service/SpaceService.java
@@ -1,7 +1,15 @@
 package com.yupi.yupicturebackend.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yupi.yupicturebackend.model.dto.space.SpaceAddRequest;
+import com.yupi.yupicturebackend.model.dto.space.SpaceQueryRequest;
 import com.yupi.yupicturebackend.model.entity.Space;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.yupi.yupicturebackend.model.entity.User;
+import com.yupi.yupicturebackend.model.vo.SpaceVO;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
 * @author hqqhqq
@@ -10,4 +18,13 @@ import com.baomidou.mybatisplus.extension.service.IService;
 */
 public interface SpaceService extends IService<Space> {
 
+    void validSpace(Space space, boolean add);
+
+    Page<SpaceVO> getSpaceVOPage(Page<Space> spacePage, HttpServletRequest request);
+
+    SpaceVO getSpaceVO(Space space, HttpServletRequest request);
+
+    void fillSpaceBySpaceLevel(Space space);
+
+    QueryWrapper<Space> getQueryWrapper(SpaceQueryRequest spaceQueryRequest);
 }
