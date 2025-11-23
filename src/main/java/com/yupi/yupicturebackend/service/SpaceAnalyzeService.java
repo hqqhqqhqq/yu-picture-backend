670bf1013cac2971486e45da3fc484a499f8d4dd
hqqhqqhqq <764238492@qq.com>
2025-11-23 16:28:01 +0800

    图库分析功能开发

--- src/main/java/com/yupi/yupicturebackend/service/SpaceAnalyzeService.java
+++ src/main/java/com/yupi/yupicturebackend/service/SpaceAnalyzeService.java
@@ -1,7 +1,36 @@
 package com.yupi.yupicturebackend.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.yupi.yupicturebackend.model.dto.space.analyze.*;
 import com.yupi.yupicturebackend.model.entity.Space;
+import com.yupi.yupicturebackend.model.entity.User;
+import com.yupi.yupicturebackend.model.vo.space.analyze.*;
+
+import java.util.List;
 
 public interface SpaceAnalyzeService extends IService<Space> {
+
+  /**
+   * 获取空间使用情况统计
+   * @param spaceUsageAnalyzeRequest
+   * @param loginUser
+   * @return
+   */
+  SpaceUsageAnalyzeResponse getSpaceUsageAnalyze(SpaceUsageAnalyzeRequest spaceUsageAnalyzeRequest, User loginUser);
+
+  /**
+   * 获取空间分类统计
+   * @param spaceCategoryAnalyzeRequest
+   * @param loginUser
+   * @return
+   */
+  List<SpaceCategoryAnalyzeResponse> getSpaceCategoryAnalyze(SpaceCategoryAnalyzeRequest spaceCategoryAnalyzeRequest, User loginUser);
+
+  List<SpaceTagAnalyzeResponse> getSpaceTagAnalyze(SpaceTagAnalyzeRequest spaceTagAnalyzeRequest, User loginUser);
+
+  List<SpaceSizeAnalyzeResponse> getSpaceSizeAnalyze(SpaceSizeAnalyzeRequest spaceSizeAnalyzeRequest, User loginUser);
+
+  List<SpaceUserAnalyzeResponse> getSpaceUserAnalyze(SpaceUserAnalyzeRequest spaceUserAnalyzeRequest, User loginUser);
+
+  List<Space> getSpaceRankAnalyze(SpaceRankAnalyzeRequest spaceRankAnalyzeRequest, User loginUser);
 }
